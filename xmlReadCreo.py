2bb0bfcbfd8f0ce2c2a0343fd5edc52e226139ad
lassefyr <latefyr@outlook.com>
2024-08-19 10:20:19 +0300

    Add files via upload
    Quick fix "String index out of range" in line 133

--- xmlReadCreo.py
+++ xmlReadCreo.py
@@ -12,13 +12,14 @@
 # This has been tested only with Creo 4.0 070
 #
 """
-    @package
-    Generate a net list file.
+	@package
+	Generate a net list file.
 
-    Command line: 
-    Run from Kicad eeschema with default parameters "%I" "%O"
+	Command line: 
+	Run from Kicad eeschema with default parameters "%I" "%O"
 	
 	Changes:
+	2024.08.19	Complaned about -string index out of range-. Fast Fix on line 133.
 	2023.05.01	Kicad 7.0.2 hierarchical refdes issue patch. Not tested to work with all combinations.
 	2022.03.01	Previous update broke V5 operation. Fixed.
 	2022.01.01	added preliminary support for KicadV6
@@ -95,7 +96,7 @@ class xmlReadCreo:
 									self.writeErrorStr( "Refdes \""+refDes+"\" does not exist! Not routed yet?\n" )
 									continue
 								roundedIntLen = math.ceil(float(self.wireLength[myindex]))
-								roundedWireLen = str(roundedIntLen).split('.')[0] 				# Round up and no decimal places
+								roundedWireLen = str(roundedIntLen).split('.')[0]				# Round up and no decimal places
 								self.writeInfoStr("Name: " + "{0:<6}".format(refDes) + " Harness Name: " + "{0:<15}".format(self.harnessNum[myindex]) + " lenght: " +roundedWireLen + "\n")
 								field['ref'] = "\""+roundedWireLen+"mm\""
 							if field[key] == "\"Harness_name\"":
@@ -124,9 +125,13 @@ class xmlReadCreo:
 	def find_RefDes( self, listItem ):
 		from sexpdata import Symbol, car, cdr
 		#self.recursiveCtr+=1
-		for i, j in  enumerate(listItem):		
+		for i, j in	enumerate(listItem):
+			print(listItem)
+			
 			if ( not hasattr(j, "__getitem__") ):
-				return				
+				return
+			elif len(j) == 0:					# Check that not empty
+				return
 			elif ( isinstance( car(j), Symbol ) and ( car(j) != Symbol("reference")) ):		
 				if( hasattr(j, "__getitem__") ):
 					self.find_RefDes( j )
@@ -146,11 +151,11 @@ class xmlReadCreo:
 				#---------------------------------------------------
 				# Check if this item is a cable or a wire. If not then continue
 				#---------------------------------------------------
-					if ( (car(y) == Symbol('property')) and  ( cdr(y)[0]== "Reference" )): 
+					if ( (car(y) == Symbol('property')) and	 ( cdr(y)[0]== "Reference" )): 
 						if( (cdr(y)[1][:1] == 'W') or (cdr(y)[1][:3]=="CBL")):							
 							refDes = cdr(y)[1]
 							# Find instance reference designator
-							for d, z in  enumerate(x):
+							for d, z in	 enumerate(x):
 								if ( car(z) == Symbol('instances') ):
 									#self.recursiveCtr=0
 									self.lastRefDes = ""
@@ -184,7 +189,7 @@ class xmlReadCreo:
 								self.writeErrorStr( "Refdes \""+refDes+"\" does not exist! Not routed yet?\n" )
 								continue
 							roundedIntLen = math.ceil(float(self.wireLength[myindex]))
-							roundedWireLen = str(roundedIntLen).split('.')[0] 				# Round up and no decimal places
+							roundedWireLen = str(roundedIntLen).split('.')[0]				# Round up and no decimal places
 							self.writeInfoStr( "Name: " + "{0:<6}".format(refDes) + " Harness Name: " + "{0:<15}".format(self.harnessNum[myindex]) + " lenght: " +roundedWireLen + "\n" )
 							y[2] = (roundedWireLen+"mm")
 	
@@ -217,8 +222,8 @@ class xmlReadCreo:
 		self.wireLength = []
 			
 		self.writeInfoStr( "Creo Back Annotation - Lengths and Harness Names\n" )								
-		self.writeInfoStr( "------------------------------------------------\n" )	
-		
+		self.writeInfoStr( "------------------------------------------------\n" )								
+
 		files = glob.glob( file_path + '.*' )
 		sorted_files = sorted(files, key=lambda x: os.path.splitext(x)[1], reverse=True )
 
@@ -281,7 +286,6 @@ class xmlReadCreo:
 							self.wireLength.append(harnessName)
 						if( varName == "HARNESS_NAME"):
 							self.harnessNum.append(harnessName)
-							
 		return True
 		
 	def is_file_newer_than_one_hour( self, file_path ):
@@ -307,7 +311,7 @@ class xmlReadCreo:
 		time_difference_in_hours = time_difference.total_seconds() / 3600
 
 		return time_difference_in_hours < 1
-		
+
 #-----------------------------------------------------------------------------------------
 # Read existing sheetnames in the design
 #
@@ -527,8 +531,8 @@ class xmlReadCreo:
 # Create instance and call with parameters
 #
 #-----------------------------------------------------------------------------------------				
-if __name__ == '__main__':      
-	fileToProcess = sys.argv[1]    				# unpack 2 command line arguments  
+if __name__ == '__main__':		
+	fileToProcess = sys.argv[1]					# unpack 2 command line arguments  
 	
 	# Split the file extension away if it exists. This comes from command line
 	# fileNameToProcess = os.path.splitext(fileToProcess)[0]
